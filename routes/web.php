b9bc29e2b3b6d99078d3263d2c4ec1356f6eb658
iqbalmaulanaputra <iqbalputra090303@gmail.com>
2026-08-01 03:14:16 +0700

    new update route

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SaveController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('posts.index');
+Route::middleware('guest')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
 });
 
-Route::get('/profile', function () {
-    return view('profile.index');
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::post('/post/{post}/save', [SaveController::class, 'toggle'])->name('posts.save');
+    Route::post('/post/{post}/like', [LikeController::class, 'toggle'])->name('posts.like');
+    Route::post('/post/{post}/comments', [CommentController::class, 'store'])->name('posts.comments.store');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.show');
 });
+
+Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('/explore', function () {
     return view('explore.index');
