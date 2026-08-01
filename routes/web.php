7f447417de55bb960534acecb61105cfb11e2f0d
iqbalmaulanaputra <iqbalputra090303@gmail.com>
2026-08-01 10:33:37 +0700

    feat: update web  and console routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\LikeController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StoryController;
+use App\Http\Controllers\FollowController;
+use App\Http\Controllers\ExploreController;
 
 Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login');
@@ -21,6 +24,11 @@ Route::middleware('auth')->group(function () {
     Route::post('/post/{post}/comments', [CommentController::class, 'store'])->name('posts.comments.store');
     Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
+    Route::post('/stories', [StoryController::class, 'store'])->name('stories.store');
+    Route::post('/story/{story}/view', [StoryController::class, 'markViewed'])->name('stories.view');
+
+    Route::post('/user/{user}/follow', [FollowController::class, 'toggle'])->name('users.follow');
+
     Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.show');
@@ -28,6 +36,4 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('/explore', function () {
-    return view('explore.index');
-});
+Route::get('/explore', [ExploreController::class, 'index'])->name('explore');
